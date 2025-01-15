1af296b04e96ca72096f323f6148150e9074d29a
jimiyeh <jimiyeh913@gmail.com>
2025-01-15 09:35:30 +0800

    [feat] 異常捕獲機制

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,32 @@
 import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:tmdb/flavors.dart';
-import 'app.dart';
 import 'package:tmdb/firebase_options/firebase_options_dev.dart' as dev;
 import 'package:tmdb/firebase_options/firebase_options_prod.dart' as prod;
+import 'package:tmdb/flavors.dart';
+import 'package:tmdb/utils/logger.dart';
+
+import 'app.dart';
 
 FutureOr<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initializeFirebaseApp();
-  runApp(const App());
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await initializeFirebaseApp();
+
+    var onError = FlutterError.onError;
+    FlutterError.onError = (details) {
+      logger.e('Error: ${details.exceptionAsString()}');
+      onError?.call(details);
+      FirebaseCrashlytics.instance.recordError(details.exception, details.stack);
+    };
+
+    runApp(const App());
+  }, (error, stackTrace) {
+    logger.e('Error: $error');
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
 }
 
 Future<void> initializeFirebaseApp() async {
---
+++ lib/utils/logger.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/foundation.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger(level: kDebugMode ? Level.all : Level.off);
