00bfff4a5632f53dcb9a355d96e521cdd98d7c69
jimiyeh <jimiyeh913@gmail.com>
2025-01-16 09:31:12 +0800

    [feat] api provider

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,7 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tmdb/model/resp_movie_list.dart';
 import 'package:tmdb/repository/api_response.dart';
+import 'package:tmdb/repository/network/network_repository.dart';
 
 abstract interface class IRepository {
   Future<ApiResponse<MovieList>> fetchNowPlayingMovieList({required int page, CancelToken? cancelToken});
 }
+
+final apiRepositoryProvider = Provider<IRepository>((ref) => NetworkRepository(ref));
